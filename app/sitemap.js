a1d7da014ca9f842cdefe62cef8fb593cd0f60e7
Anandu Jayakumar <anandujay18@gmail.com>
2026-08-16 16:27:07 +0530

    Add sitemap and robots.txt

---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = 'https://thretha.in'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/shop`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+  ]
+}
